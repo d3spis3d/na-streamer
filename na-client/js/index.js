5749236581867f8adfccdeb6061cfa3c482af61c
Daniel Budden <thedb317@hotmail.com>
2015-12-17 14:08:57 +1100

    Start testing music file traversal

--- na-client/js/index.js
+++ na-client/js/index.js
@@ -1,1 +1,21 @@
-import express from 'express';
+import dir from 'node-dir';
+import path from 'path';
+
+const dirPath = '/path/to/music';
+
+dir.files(dirPath, function(err, files) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    if (files) {
+        files.forEach((file) => {
+            console.log('file:', file);
+            const [artist, album, songFile] = path.relative(dirPath, file).split(path.sep);
+            console.log('artist:', artist);
+            console.log('album:', album);
+            console.log('song file:', songFile);
+        });
+    }
+});
