b8c96ee75d28261561a57d4ac46fb09494aca28e
Daniel Budden <daniel.l.budden@gmail.com>
2015-12-20 21:04:20 +1100

    Add dir watch with file created/removed events

--- na-client/js/index.js
+++ na-client/js/index.js
@@ -1,4 +1,5 @@
 import dir from 'node-dir';
+import watch from 'watch';
 import path from 'path';
 import commandLineArgs from 'command-line-args';
 
@@ -11,6 +12,14 @@ const cli = commandLineArgs([
 ]);
 const options = cli.parse();
 
+function processFile(file) {
+    console.log('file:', file);
+    const [artist, album, songFile] = path.relative(options.dir, file).split(path.sep);
+    console.log('artist:', artist);
+    console.log('album:', album);
+    console.log('song file:', songFile);
+}
+
 dir.files(options.dir, function(err, files) {
     if (err) {
         console.log(err);
@@ -19,11 +28,17 @@ dir.files(options.dir, function(err, files) {
 
     if (files) {
         files.forEach((file) => {
-            console.log('file:', file);
-            const [artist, album, songFile] = path.relative(options.dir, file).split(path.sep);
-            console.log('artist:', artist);
-            console.log('album:', album);
-            console.log('song file:', songFile);
+            processFile(file);
         });
     }
+});
+
+watch.createMonitor(options.dir, function(monitor) {
+    monitor.on('created', function(file) {
+        processFile(file);
+    });
+
+    monitor.on('removed', function(file) {
+        processFile(file);
+    });
 });
