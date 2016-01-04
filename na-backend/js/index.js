f0d3098a01b3270354aa4e8de4124af1589c511d
Daniel Budden <daniel.l.budden@gmail.com>
2016-01-04 17:16:21 +1100

    Setup backend/client for test connection with frontend

--- na-backend/js/index.js
+++ na-backend/js/index.js
@@ -17,55 +17,58 @@ app.get('/:uuid', function(req, res) {
     res.sendStatus(200);
 });
 
-const appServer = app.listen(3000, function () {
+const appServer = app.listen(4000, function () {
   const host = appServer.address().address;
   const port = appServer.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-});
 
-const streamerServer = BinaryServer({port: 9000});
+  const streamerServer = BinaryServer({port: 9000});
 
-streamerServer.on('connection', function(streamer) {
-    const streamerId = uuid.v4();
-    streamer.on('stream', function(stream) {
-        streamers[streamerId] = stream;
+  streamerServer.on('connection', function(streamer) {
+      const streamerId = uuid.v4();
+      streamer.on('stream', function(stream) {
+          streamers[streamerId] = stream;
 
-        const streamedData = Rx.Observable.fromEvent(stream, 'data');
+          const streamedData = Rx.Observable.fromEvent(stream, 'data');
 
-        const trackListingData = streamedData.filter(data => !Buffer.isBuffer(data) && !(typeof data === 'string' || data instanceof String));
-        trackListingData.subscribe(data => {
-            for (let artist in data) {
-                tracks[artist] = tracks[artist] || {};
-                for (let albumName in data[artist]) {
-                    tracks[artist][albumName] = tracks[artist][albumName] || {};
-                    const album = data[artist][albumName];
-                    for (let track in album) {
-                        filesByStreamer[album[track].id] = streamerId;
-                        tracks[artist][albumName][track] = album[track];
-                    }
-                }
-            }
-            console.log(filesByStreamer);
-        });
+          const trackListingData = streamedData.filter(data => !Buffer.isBuffer(data) && !(typeof data === 'string' || data instanceof String));
+          trackListingData.subscribe(data => {
+              for (let artist in data) {
+                  tracks[artist] = tracks[artist] || {};
+                  for (let albumName in data[artist]) {
+                      tracks[artist][albumName] = tracks[artist][albumName] || {};
+                      const album = data[artist][albumName];
+                      for (let track in album) {
+                          filesByStreamer[album[track].id] = streamerId;
+                          tracks[artist][albumName][track] = album[track];
+                      }
+                  }
+              }
+              console.log(filesByStreamer);
+          });
 
-        const trackEnd = streamedData.filter(data => !Buffer.isBuffer(data) && (typeof data === 'string' || data instanceof String));
-        trackData.subscribe(data => {
-            console.log('track finished', data);
-        });
+          const trackEnd = streamedData.filter(data => !Buffer.isBuffer(data) && (typeof data === 'string' || data instanceof String));
+          trackEnd.subscribe(data => {
+              console.log('track finished', data);
+          });
 
-        const binaryData = streamedData.filter(data => Buffer.isBuffer(data));
-        binaryData.subscribe(data => {
-            console.log('received binary data');
-            clients.forEach(client => {
-                client.stream.write(data);
-            });
-        });
-    });
-});
+          const binaryData = streamedData.filter(data => Buffer.isBuffer(data));
+          binaryData.subscribe(data => {
+              console.log('received binary data');
+              clients.forEach(client => {
+                  if (client.stream.writable) {
+                      client.stream.write(data);
+                  }
+              });
+          });
+      });
+  });
 
-const frontendServer = BinaryServer({server: app, path: '/api/v1/binary-data'});
-frontendServer.on('connection', function(client) {
-    const stream = client.createStream();
-    clients.push({stream: stream});
+  const frontendServer = BinaryServer({port: 9001});
+  frontendServer.on('connection', function(client) {
+      client.on('stream', function(stream) {
+          clients.push({stream: stream});
+      });
+  });
 });
