d105918a47d7c9fad731ffc672681e778bd3f5c5
Daniel Budden <daniel.l.budden@gmail.com>
2016-01-04 21:08:21 +1100

    Setup frontend so it plays the stream somewhat

--- na-frontend/js/app.js
+++ na-frontend/js/app.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', () => {
     const client = new BinaryClient(`ws://localhost:9001`);
     const audioCtx = new window.AudioContext();
-    const audioBuffer = audioCtx.createBuffer(1, 8192, 44100);
-    let playing = false;
 
     client.on('open', function() {
         console.log('client opened');
@@ -10,17 +8,13 @@ window.addEventListener('load', () => {
 
         stream.on('data', (data) => {
             console.log('got data');
-            const buffer = new Float32Array(data);
-            console.log(buffer.length);
-            audioBuffer.copyToChannel(buffer, 0, 0);
-
-            if (!playing) {
+            audioCtx.decodeAudioData(data, buffer => {
+                console.log(buffer.length);
                 const source = audioCtx.createBufferSource();
-                source.buffer = audioBuffer;
+                source.buffer = buffer;
                 source.connect(audioCtx.destination);
                 source.start();
-                playing = true;
-            }
+            });
         });
     });
 });
