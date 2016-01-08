cfa3ffa8ec91bca5ffd4ba255298dfb43e77237a
Daniel Budden <daniel.l.budden@gmail.com>
2016-01-08 17:43:56 +1100

    Refactor client and add more tests

--- na-client/js/lib/client.js
+++ na-client/js/lib/client.js
@@ -1,26 +1,13 @@
-import fs from 'fs';
-
 import {BinaryClient} from 'binaryjs';
-import {setupFilesProcessing} from './files';
-import Rx from 'rx';
-import probe from 'node-ffprobe';
-import Throttle from 'throttle';
+
+import * as streams from './streams';
+
 
 export function startClient(filesStore, musicDir) {
     const client = BinaryClient('ws://localhost:9000');
 
     client.on('open', function() {
-        console.log('opened connection');
-        const stream = client.createStream();
-        const sendFileData = createStreamToServer(stream);
-        const streamTrack = createTrackRequestResponse(filesStore, sendFileData);
-
-        const streamedData = Rx.Observable.fromEvent(stream, 'data');
-
-        const trackRequests = streamedData.filter(data => typeof data === 'string' || data instanceof String);
-        trackRequests.subscribe(streamTrack);
-
-        setupFilesProcessing(filesStore, sendFileData, musicDir);
+        streams.openStreamToServer(filesStore, musicDir, client);
     });
     client.on('close', function() {
         console.log('connection closed');
@@ -28,41 +15,6 @@ export function startClient(filesStore, musicDir) {
     client.on('error', function(err) {
         console.log('error: ', err);
     });
-}
 
-export function createStreamToServer(stream) {
-    return function(hostedFileData) {
-        stream.write(hostedFileData);
-    };
-}
-
-export function createTrackRequestResponse(filesStore, sendFileData) {
-    return function(trackId) {
-        console.log(trackId);
-        const track = filesStore[trackId];
-        const streamTrack = createTrackStream(track, trackId, sendFileData);
-        probe(track, streamTrack);
-    };
-}
-
-export function createTrackStream(track, trackId, sendFileData) {
-    return function(err, results) {
-        const trackStream = fs.createReadStream(track);
-        const bps = (results.format.bit_rate / 10) * 1.4;
-        const chunkSize = Math.ceil(bps / 3);
-        const throttledStream = new Throttle({bps, chunkSize});
-        trackStream.pipe(throttledStream);
-
-        const throttledData = Rx.Observable.fromEvent(throttledStream, 'data')
-        .subscribe(data => {
-            sendFileData(data);
-        });
-
-        const throttleEnd = Rx.Observable.fromEvent(throttledStream, 'end')
-        .subscribe(() => {
-            throttledData.dispose();
-            throttleEnd.dispose();
-            sendFileData(trackId);
-        });
-    };
+    return client;
 }
--- na-client/test/lib/client-test.js
+++ na-client/test/lib/client-test.js
@@ -1,94 +1,27 @@
-import {createStreamToServer, createTrackRequestResponse, createTrackStream} from '../../js/lib/client';
+import * as streams from '../../js/lib/streams';
+import {startClient} from '../../js/lib/client';
+
+import {EventEmitter} from 'events';
+import path from 'path';
+
 import {expect} from 'chai';
 import sinon from 'sinon';
-import path from 'path';
-import probe from 'node-ffprobe';
-import Throttle from 'throttle';
+import binaryjs from 'binaryjs';
+
 
 describe('Client functions', function() {
-    describe('createStreamToServer', function() {
-        it('should return a function', function() {
-            const input = {};
-
-            const results = createStreamToServer(input);
-
-            expect(results).to.be.a('function');
-        });
-
-        it('should create a function that writes to stream', function() {
-            const stream = {
-                write: function() { }
-            };
-            const spy = sinon.spy(stream, 'write');
-            const inputData = { key: 'value' };
-
-            const func = createStreamToServer(stream);
-            func(inputData);
-
-            expect(spy.calledWith(inputData)).to.be.true;
-        });
-    });
-
-    describe('createTrackRequestResponse', function() {
-        it('should return a function', function() {
+    describe('startClient', function() {
+        it('should call openStreamToServer on open event', function() {
             const filesStore = {};
-            const sendFileData = function() { };
+            const musicDir = ['home', 'test', 'music'].join(path.sep);
+            const clientSpy = sinon.stub(binaryjs, 'BinaryClient').returns(new EventEmitter());
+            const openStreamSpy = sinon.stub(streams, 'openStreamToServer');
 
-            const results = createTrackRequestResponse(filesStore, sendFileData);
+            const client = startClient(filesStore, musicDir);
 
-            expect(results).to.be.a('function');
-        });
+            client.emit('open');
 
-        it('should call create track stream and pass probe results to function', function() {
-            const track = ['home', 'music', 'Artist', 'Album', '01-Song.mp3'].join(path.sep);
-            const trackId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-            const filesStore = {};
-            filesStore[trackId] = track;
-            const sendFileData = function() { };
-
-            const probeSpy = sinon.spy(probe);
-            const streamSpy = sinon.spy(createTrackStream);
-
-            const streamTrack = createTrackRequestResponse(filesStore, sendFileData);
-            streamTrack(trackId);
-
-            expect(streamSpy.calledWith(track, trackId, sendFileData));
-            expect(probeSpy.calledWith(track, sinon.match.func));
-        });
-    });
-
-    describe('createTrackStream', function() {
-        it('should return a function', function() {
-            const track = ['home', 'music', 'Artist', 'Album', '01-Song.mp3'].join(path.sep);
-            const trackId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-            const sendFileData = function() { };
-
-            const results = createTrackStream(track, trackId, sendFileData);
-
-            expect(results).to.be.a('function');
-        });
-
-        it('should return a function that creates throttled stream', function() {
-            const err = null;
-            const input = {
-                format: {
-                    bit_rate: 100
-                }
-            };
-            const track = ['home', 'music', 'Artist', 'Album', '01-Song.mp3'].join(path.sep);
-            const trackId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-            const sendFileData = function() { };
-
-            const throttleSpy = sinon.spy(Throttle);
-
-            const streamTrack = createTrackStream(track, trackId, sendFileData);
-
-            streamTrack(err, input);
-
-            expect(throttleSpy.calledWith({
-                bps: 14,
-                chunkSize: 5
-            }));
+            expect(openStreamSpy.calledWith(filesStore, musicDir, client));
         });
     });
 });
