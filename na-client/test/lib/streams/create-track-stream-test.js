07079df752a436ce2baf4146f57d359b8980ce49
Daniel Budden <daniel.l.budden@gmail.com>
2016-01-12 10:03:01 +1100

    Additional client test

--- na-client/test/lib/streams/create-track-stream-test.js
+++ na-client/test/lib/streams/create-track-stream-test.js
@@ -18,7 +18,7 @@ describe('createTrackStream', function() {
         expect(results).to.be.a('function');
     });
 
-    it('should return a function that creates throttled stream', function() {
+    it('should return a function that creates throttled stream and sends file data', function() {
         const err = null;
         const input = {
             format: {
@@ -46,5 +46,35 @@ describe('createTrackStream', function() {
         })).to.be.true;
 
         expect(sendFileData.calledWith(mockData));
+    });
+
+    it('should return a function that send track id on data stream end', function() {
+        const err = null;
+        const input = {
+            format: {
+                bit_rate: 100
+            }
+        };
+        const track = ['home', 'music', 'Artist', 'Album', '01-Song.mp3'].join(path.sep);
+        const trackId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+        const mockData = 'mock data';
+
+        const sendFileData = sinon.spy();
+        const throttleStream = new EventEmitter();
+        const throttleSpy = sinon.stub().returns(throttleStream);
+
+        const streamTrack = createTrackStream(track, trackId, sendFileData, throttleSpy);
+
+        streamTrack(err, input);
+
+        throttleStream.emit('end');
+
+        expect(throttleSpy.calledWithNew()).to.be.true;
+        expect(throttleSpy.calledWith({
+            bps: 14,
+            chunkSize: 5
+        })).to.be.true;
+
+        expect(sendFileData.calledWith(trackId));
     });
 });
