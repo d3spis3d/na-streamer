adf62d536c62828fcd17b0da7b033f579e1164b8
Daniel Budden <daniel.l.budden@gmail.com>
2016-01-17 00:40:33 +1100

    Create Artist, Album, Song and edges between

--- na-backend/js/lib/server-helper.js
+++ na-backend/js/lib/server-helper.js
@@ -15,28 +15,99 @@ export function setClientList(clients) {
 
 export function setTrackListingMap(tracks, filesByStreamer, streamerId, db) {
     return function(data) {
-        console.log('update tracks listing called');
         for (let artist in data) {
             tracks[artist] = tracks[artist] || {};
-            for (let albumName in data[artist]) {
-                tracks[artist][albumName] = tracks[artist][albumName] || {};
-                const album = data[artist][albumName];
-                for (let track in album) {
-                    filesByStreamer[album[track].id] = streamerId;
-                    tracks[artist][albumName][track] = album[track];
-                    console.log('querying database');
-                    db.query('insert into Song (title) values (:title)', {
+            db.query('select * from Artist where name=:name', {
+                params: {
+                    name: artist
+                }
+            })
+            .then((artistResults) => {
+                if (artistResults.length) {
+                    return;
+                }
+                return db.query('insert into Artist (name) values (:name)', {
+                    params: {
+                        name: artist
+                    }
+                });
+            })
+            .then(() => {
+                for (let albumName in data[artist]) {
+                    tracks[artist][albumName] = tracks[artist][albumName] || {};
+                    db.query('select * from Album where title=:title', {
                         params: {
-                            title: album[track].title
+                            title: albumName
                         }
                     })
-                    .then((response) => {
-                        console.log('Inserted:', response)
-                    });
+                    .then(albumResults => {
+                        if (albumResults.length) {
+                            return
+                        }
+                        return db.query('insert into Album (title) values (:title)', {
+                            params: {
+                                title: albumName
+                            }
+                        });
+                    })
+                    .then(() => {
+                        db.let('firstVertex', s => {
+                            s.select()
+                            .from('Album')
+                            .where({'title': albumName});
+                        })
+                        .let('secondVertex', s => {
+                            s.select()
+                            .from('Artist')
+                            .where({'name': artist});
+                        })
+                        .let('joiningEdge', s => {
+                            s.create('edge', 'Recorded_By')
+                            .from('$firstVertex')
+                            .to('$secondVertex');
+                        })
+                        .commit()
+                        .return('$firstVertex')
+                        .all()
+                        .then((results) => {
+                            createSongs(data, artist, albumName, filesByStreamer, tracks, db, streamerId);
+                        });
+                    })
                 }
-            }
+            });
         }
     };
+}
+
+function createSongs(data, artist, albumName, filesByStreamer, tracks, db, streamerId) {
+    const album = data[artist][albumName];
+    for (let track in album) {
+        filesByStreamer[album[track].id] = streamerId;
+        tracks[artist][albumName][track] = album[track];
+        db.let('firstVertex', s => {
+            s.select()
+            .from('Album')
+            .where({'title': albumName});
+        })
+        .let('secondVertex', s => {
+            s.create('vertex', 'Song')
+            .set({
+                title: album[track].title,
+                number: album[track].number
+            });
+        })
+        .let('joiningEdge', s => {
+            s.create('edge', 'Found_On')
+            .from('$secondVertex')
+            .to('$firstVertex');
+        })
+        .commit()
+        .return('$secondVertex')
+        .all()
+        .then((response) => {
+            console.log('Inserted:', response)
+        });
+    }
 }
 
 export function setupInitQueue(songQueue, filesByStreamer, streamers) {
