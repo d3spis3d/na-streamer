5f941ad922765381002e402719dc1147d707cdab
Daniel Budden <daniel.l.budden@gmail.com>
2016-02-06 21:59:10 +1100

    Setup redux and create first action and reducer

--- na-frontend/js/app.js
+++ na-frontend/js/app.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+
 import App from './components/App';
+import rootReducer from './reducers/reducers.js';
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('main')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('main')
 );
---
+++ na-frontend/js/reducers/reducers.js
@@ -0,0 +1,31 @@
+import { combineReducers } from 'redux';
+
+import {FETCH_QUEUE, RECEIVE_QUEUE} from '../actions/actions';
+
+const initialState = {
+    loadingQueue: false,
+    queue: []
+};
+
+function queue(state = initialState, action) {
+    switch (action.type) {
+    case FETCH_QUEUE:
+        return Object.assign({}, state, {
+            loadingQueue: true,
+            queue: []
+        });
+    case RECEIVE_QUEUE:
+        return Object.assign({}, state, {
+            loadingQueue: false,
+            queue: action.queue
+        });
+    default:
+        return state;
+    }
+}
+
+const rootReducer = combineReducers([
+    queue
+]);
+
+export default rootReducer;
