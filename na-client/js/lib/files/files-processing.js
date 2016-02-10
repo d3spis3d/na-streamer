adadf397c1eec925853faf371d6f804104bcbf5e
Daniel Budden <daniel.l.budden@gmail.com>
2016-02-10 16:32:05 +1100

    Try new client files approach

--- na-client/js/lib/files/files-processing.js
+++ na-client/js/lib/files/files-processing.js
@@ -16,7 +16,61 @@ export function setupFilePathProcessor(sendFileData, processTracks, musicDir, ke
         if (files) {
             const tracks = files.filter(file => file.indexOf(CLIENT_KEYFILE) === -1)
                                 .map(extractTrack(musicDir));
-            const hostedTracks = processTracks(tracks, buildFileInfoForBackend, {});
+
+            const songs = tracks.map(track => {
+                return {
+                    title: track.title,
+                    number: track.number,
+                    id: track.id,
+                    album: track.album
+                };
+            });
+
+            const albums = tracks.reduce((map, track) => {
+                map[track.album] = map[track.album] || {
+                    title: track.album,
+                    artist: track.artist,
+                };
+                return map;
+            }, {});
+
+            const artists = tracks.reduce((map, track) => {
+                map[track.artist] = map[track.artist] || {
+                    name: track.artist,
+                    genre: track.genre,
+                };
+                return map;
+            }, {});
+
+            const genres = tracks.reduce((map, track) => {
+                map[track.genre] = map[track.genre] || {
+                    name: track.genre,
+                };
+                return map;
+            }, {});
+
+            const hostedTracks = {
+                genres: [],
+                artists: [],
+                albums: [],
+                songs: songs
+            };
+
+            for (let albumName in albums) {
+                const album = albums[albumName];
+                hostedTracks.albums.push(album);
+            }
+
+            for (let artistName in artists) {
+                const artist = artists[artistName];
+                hostedTracks.artists.push(artist);
+            }
+
+            for (let genreName in genres) {
+                const genre = genres[genreName];
+                hostedTracks.genres.push(genre);
+            }
+
             sendFileData({
                 key: key,
                 tracks: hostedTracks
