78961073e3ab02f015cccfc3d613b1e92dd6c010
Daniel Budden <daniel.l.budden@gmail.com>
2016-07-29 23:24:30 +1000

    Fix routes tests

--- na-backend/js/routes/clients.js
+++ na-backend/js/routes/clients.js
@@ -2,9 +2,11 @@ export const getClients = {
     url: '/api/clients',
     generateHandler: function(clients) {
         return function(req, res) {
-            res.status(200).send(JSON.stringify({
-                count: clients.count()
-            }));
+            res.status(200)
+                .send(JSON.stringify({
+                    count: clients.count()
+                })
+            );
         };
     }
 }
--- na-backend/test/routes/clients-test.js
+++ na-backend/test/routes/clients-test.js
@@ -3,8 +3,16 @@ import sinon from 'sinon';
 
 import {getClients} from '../../js/routes/clients';
 
+let clients;
+
 describe('Clients route', function () {
     describe('getClients', function () {
+        beforeEach(function() {
+            clients = {
+                count: sinon.stub().returns(5)
+            };
+        })
+
         it('should have correct url', function () {
             const expectedUrl = '/api/clients';
 
@@ -12,25 +20,18 @@ describe('Clients route', function () {
         });
 
         it('should return a function from generateHandler', function () {
-            const clients = [];
-
             const results = getClients.generateHandler(clients);
             expect(results).to.be.a('function');
         });
 
         it('should return count of current connected clients', function () {
-            const clients = [
-                { ip: '127.0.0.1' },
-                { ip: '0.0.0.0' }
-            ];
-
             const req = {};
             const res = {
                 status: sinon.stub().returnsThis(),
                 send: sinon.stub()
             };
 
-            const expectedResponse = JSON.stringify({ count: 2});
+            const expectedResponse = JSON.stringify({ count: 5 });
 
             const handler = getClients.generateHandler(clients);
             handler(req, res);
